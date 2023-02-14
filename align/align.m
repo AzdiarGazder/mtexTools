582c33c4c101963a230a9d29fee16050cadc4481
azdiarGazder <azdiar.gazder@gmail.com>
2023-02-14 14:37:19 +1100

    Create align.m

---
+++ align/align.m
@@ -0,0 +1,162 @@
+function outebsd = align(inebsd,varargin)
+%% Function description:
+% Re-aligns ebsd map data along a user-defined linear fiducial
+% The linear fiducial may correspond to a twin boundary, stacking fault, or 
+% any linear-shaped deformation or phase transformation products.
+%
+%% Author:
+% Dr. Azdiar Gazder, 2022, azdiaratuowdotedudotau
+%
+%% Acknowledgements: 
+% Dr. David R.G. Mitchell, UOW 
+% For the original DigitalMicrograph -based script in C for image 
+% realignment along a user-defined linear fiducial
+%
+%% Syntax
+%
+%  [ebsd] = align(ebsd,numPixels)
+%
+% Input
+%  ebsd      - @EBSD
+%
+% Output
+%  ebsd      - @EBSD
+%
+% Options
+%  pixels    - number of pixels defining the halfwidth on either side of 
+%              the fiducial line
+%%
+
+
+numPixels = get_option(varargin,'pixels',15);
+
+
+% grid ebsd map data
+% While MTex's default "gridify.m" can be used here, the command creates 
+% Nan pixels.
+% It is recommended to use the modified "gridify2.m" instead.
+[gebsd,~] = gridify2(inebsd);
+
+f = figure;
+plot(gebsd,gebsd.bc)
+colormap parula
+set(f,'Name',...
+    'Band contrast map: Left-click to select two points defining a line along a linear map feature.',...
+    'NumberTitle','on');
+hold all
+xy1 = ginput(2);
+
+% check for start and end y-coordinates
+% ensure the end y-coordinate is always larger than the start y-coordinate
+xy1 = sortrows(xy1,2);
+
+% check for xy coordinates within the map bounds
+xy1((xy1(:,1)<=gebsd.xmin),1) = gebsd.xmin;
+xy1((xy1(:,1)>=gebsd.xmax),1) = gebsd.xmax;
+xy1((xy1(:,2)<=gebsd.ymin),2) = gebsd.ymin;
+xy1((xy1(:,2)>=gebsd.ymax),2) = gebsd.ymax;
+
+% calculate the closest multiple of xy values based on the map step size
+stepSize = gebsd.dx;
+xy1 = stepSize.*round(xy1./stepSize);
+
+% plot the fiducial line
+line(xy1(:,1),xy1(:,2),'color',[1 0 0 0.5],'linewidth',5)
+hold all
+
+% calculate the equation of the fiducial line
+deltaY = xy1(2,2)-xy1(1,2);
+deltaX = xy1(2,1)-xy1(1,1);
+slope = deltaY/deltaX;
+perpSlope = -1/slope;
+intercept = xy1(1,2)-(xy1(1,1)*slope);
+
+% define the start & end row indices of the fiducial line
+startRowIdx = double(uint16(xy1(1,2)/stepSize));
+endRowIdx = double(uint16(xy1(2,2)/stepSize));
+
+% define the half width for the line profile
+halfWidth = numPixels*stepSize;
+
+% define the array containing the pixel shifts
+pixelShift = zeros(fliplr(size(xy1(1,2):stepSize:xy1(2,2))));
+
+for yy = xy1(1,2):stepSize:xy1(2,2)
+    % calculate the x-position on the line profile
+    xx = (yy-intercept)/slope;
+    left = xx-halfWidth;
+    right = xx+halfWidth;
+    % check for left & right -most line profile coordinates within the map
+    % bounds
+    if left <= gebsd.xmin
+        left = gebsd.xmin;
+        right = halfWidth; %2*halfWidth;
+    end
+    if right >= gebsd.xmax
+        right = gebsd.xmax;
+        left = gebsd.xmax-halfWidth; %gebsd.xmax-(2*halfWidth);
+    end
+    % define the start and end coordinates of the line profile
+    xy2 = [left, yy;...
+        right, yy];
+    % draw the line from which the line profile information is sought
+    line(xy2(:,1),xy2(:,2),'color',[0.5 0.5 0.5 0.5],'linewidth',2.5);
+    hold all;
+    % get the line profile information
+    [gebsdLine2,~] = spatialProfile(gebsd,xy2);
+    % find the minimum band contrast value along the line profile
+    % this corresponds to the linear boundary feature
+    [~,minIdx] = min(gebsdLine2.bc);
+    % calculate current row index
+    currentRowIdx = double(uint16(yy/stepSize))+1;
+    % calculate the number of pixels to shift the column data from the
+    % center of the line profile
+    arrayIdx = currentRowIdx-startRowIdx;
+    midIdx = ceil(size(gebsdLine2.bc,1)/2);
+    pixelShift(arrayIdx,1) = midIdx-minIdx;
+
+    % circularly shift columns left or right for each of the map elements 
+    % row-wise rotations
+    gebsd.rotations(currentRowIdx,:) = circshift(gebsd.rotations(currentRowIdx,:),...
+        [0,pixelShift(arrayIdx,1)]);
+    % phase
+    gebsd.phase(currentRowIdx,:) = circshift(gebsd.phase(currentRowIdx,:),...
+        [0,pixelShift(arrayIdx,1)]);
+%     % isIndexed
+%     gebsd.isIndexed(currentRowIdx,:) = circshift(gebsd.isIndexed(currentRowIdx,:),...
+%         [0,pixelShift(arrayIdx,1)]);
+    % band contrast
+    gebsd.prop.bc(currentRowIdx,:) = circshift(gebsd.prop.bc(currentRowIdx,:),...
+        [0,pixelShift(arrayIdx,1)]);
+    % band slope
+    gebsd.prop.bs(currentRowIdx,:) = circshift(gebsd.prop.bs(currentRowIdx,:),...
+        [0,pixelShift(arrayIdx,1)]);
+    % error
+    gebsd.prop.error(currentRowIdx,:) = circshift(gebsd.prop.error(currentRowIdx,:),...
+        [0,pixelShift(arrayIdx,1)]);
+    % mad
+    gebsd.prop.mad(currentRowIdx,:) = circshift(gebsd.prop.mad(currentRowIdx,:),...
+        [0,pixelShift(arrayIdx,1)]);
+
+    progress(arrayIdx,(endRowIdx-startRowIdx+1));
+    pause(0.0001);
+end
+hold off;
+
+% phaseId
+gebsd.phaseId = reshape((gebsd.phase+1),[numel(gebsd.phase),1]);
+
+% f = figure;
+% plot(gebsd,gebsd.bc)
+% colormap parula
+% set(f,'Name',...
+% 'Band contrast map: Aligned along user-selected linear feature',...
+% 'NumberTitle','on');
+
+% un-gridify the ebsd data
+outebsd = EBSD(gebsd);
+
+close all
+end
+% %---
+
