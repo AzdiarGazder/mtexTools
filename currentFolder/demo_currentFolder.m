7bb7f43739dd23f7b6e3bc7cd623eeced29d5329
azdiarGazder <azdiar.gazder@gmail.com>
2023-03-13 12:52:01 +1100

    Create demo_currentFolder.m

---
+++ currentFolder/demo_currentFolder.m
@@ -0,0 +1,8 @@
+close all; clc; clear all; clear hidden;
+currentFolder;
+
+%% Initialize MTEX
+% Startup and set some settings
+startup_mtex;
+setMTEXpref('FontSize',14);
+set(0,'DefaultFigureWindowStyle','normal');
