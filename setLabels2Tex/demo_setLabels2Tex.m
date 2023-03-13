38ff128ca1a678c32919726fc97d9a93bdb60318
azdiarGazder <azdiar.gazder@gmail.com>
2023-03-14 09:58:41 +1100

    Create demo_setLabels2Tex.m

---
+++ setLabels2Tex/demo_setLabels2Tex.m
@@ -0,0 +1,8 @@
+close all; clc; clear all; clear hidden;
+
+%% Initialize MTEX
+% Startup and set some settings
+startup_mtex;
+setMTEXpref('FontSize',14);
+set(0,'DefaultFigureWindowStyle','normal');
+setLabels2Tex;
