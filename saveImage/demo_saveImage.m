b2ee3a02d08bc8be30d54e177a997e71e2590555
azdiarGazder <azdiar.gazder@gmail.com>
2023-03-15 09:31:47 +1100

    Create demo_saveImage.m

---
+++ saveImage/demo_saveImage.m
@@ -0,0 +1,28 @@
+close all; clc; clear all; clear hidden;
+
+%% Initialize MTEX
+% Startup and set some settings
+startup_mtex;
+setMTEXpref('FontSize',14);
+set(0,'DefaultFigureWindowStyle','normal');
+
+
+% %-----------------
+setMTEXpref('xAxisDirection','north');
+setMTEXpref('zAxisDirection','outofPlane');
+% define the crystal system
+CS = {'notIndexed',...
+    crystalSymmetry('6/mmm', [1.5708 1.5708 2.0944], 'X||a*', 'Y||b', 'Z||c*', 'mineral', 'Mg')};
+% load an mtex dataset
+mtexdata twins
+ebsd.CSList = CS;
+% %-----------------
+
+
+figure
+plot(ebsd,ebsd.bc)
+
+figure
+plot(ebsd,ebsd.orientations)
+
+saveImage
