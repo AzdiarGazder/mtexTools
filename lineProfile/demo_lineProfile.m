d7bc468a026c44ec73b2c3a1d8f1532e2b87633b
azdiarGazder <azdiar.gazder@gmail.com>
2023-03-16 10:20:05 +1100

    Create demo_lineProfile.m

---
+++ lineProfile/demo_lineProfile.m
@@ -0,0 +1,38 @@
+close all; clc; clear all; clear hidden;
+startup_mtex
+
+% %-----------------
+% % hexGrid
+% % define the crystal system
+% CS = {'notIndexed',...
+%     crystalSymmetry('m-3m', [3.6 3.6 3.6], 'mineral', 'copper')};
+% % load an mtex dataset
+% mtexdata copper
+% ebsd.CSList = CS;
+% % consider only indexed data
+% ebsd = ebsd('indexed');
+% ebsd = ebsd('copper');
+% 
+% figure; 
+% lineProfile(ebsd,ebsd.orientations,'color',[0 0 0],'linestyle','-');
+% % -----------------
+
+
+
+%-----------------
+% squareGrid
+% define the crystal system
+CS = {'notIndexed',...
+    crystalSymmetry('6/mmm', [1.5708 1.5708 2.0944], 'X||a*', 'Y||b', 'Z||c*', 'mineral', 'Mg')};
+% load an mtex dataset
+mtexdata twins
+ebsd.CSList = CS;
+% consider only indexed data
+ebsd = ebsd('indexed');
+ebsd = ebsd('Mg');
+[grains,ebsd.grainId] = calcGrains(ebsd,'angle',2*degree);
+
+lineProfile(ebsd,ebsd.orientations,'color',[0 0 0],'linestyle','-');
+% lineProfile(ebsd,ebsd.bc,'color',[0 0 0],'linestyle','--');
+% lineProfile(ebsd,ebsd.bands,'color',[0 0 0],'linestyle',':');
+%-----------------
