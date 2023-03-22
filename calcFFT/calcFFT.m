beb63692fdb74ecae40d2fc4693ac8f3b3efdfb5
azdiarGazder <azdiar.gazder@gmail.com>
2023-03-22 16:09:04 +1100

    Update calcFFT.m

--- calcFFT/calcFFT.m
+++ calcFFT/calcFFT.m
@@ -1,7 +1,9 @@
-function outgrains = calcFFT(inebsd,ingrains)
+function outgrains = calcFFT(inebsd,ingrains,varargin)
 %% Function description:
-% Returns the Fast Fourier Transforms (FFTs) of individual grains.
-% The real and complex FFTs are returned in grid format within the 
+% Returns the Fast Fourier Transforms (FFTs) of individual grains. Unless
+% specified otherwise, the FFTs are calculated after padding the binary
+% grain map/image to its nearest square.
+% The real and complex FFTs are returned in grid format within the
 % 'grains.prop.fftReal' and 'grains.prop.fftComplex' structure variables.
 %
 %% Author:
@@ -16,12 +18,18 @@ function outgrains = calcFFT(inebsd,ingrains)
 %
 %% Output:
 %  grains       - @grain2d
+%
+%% Options:
+%  'noPad'     -  @char, specifies no padding of the binary grain map/image
 %%
 
+
+% check for variable content
 if isempty(inebsd) || isempty(ingrains)
-    return; 
+    return;
 end
 
+% check for variable type
 if ~isa(inebsd,'EBSD')
     error('To create FFTs, first input must be an EBSD variable.');
     return;
@@ -31,6 +39,15 @@ if ~isa(ingrains,'grain2d')
     error('To create FFTs, second input must be a grain2d variable.');
     return;
 end
+
+% check for padding of the binary grain map/image before FFT
+if ~isempty(varargin) && check_option(varargin,'noPad')
+    padLogic = false;
+else
+    padLogic = true;
+end
+
+
 
 outgrains = ingrains;
 
@@ -51,56 +68,57 @@ for ii = 1:length(outgrains)
         binaryImg = ggrain.prop.bandslope;
     elseif any(ismember(fields(ggrain.prop),'bs'))
         binaryImg = ggrain.prop.bs;
-%     elseif any(ismember(fields(ggrain.prop),'oldId'))
-%         binaryImg = ggrain.prop.oldId;
-%     elseif any(ismember(fields(ggrain.prop),'grainId'))
-%         binaryImg = ggrain.prop.grainId;
-%     elseif any(ismember(fields(ggrain.prop),'confidenceindex'))
-%         binaryImg = ggrain.prop.confidenceindex;
-%     elseif any(ismember(fields(ggrain.prop),'ci'))
-%         binaryImg = ggrain.prop.ci;
-%     elseif any(ismember(fields(ggrain.prop),'fit'))
-%         binaryImg = ggrain.prop.fit;
-%     elseif any(ismember(fields(ggrain.prop),'semsignal'))
-%         binaryImg = ggrain.prop.semsignal;
-%     elseif any(ismember(fields(ggrain.prop),'mad'))
-%         binaryImg = ggrain.prop.mad;
-%     elseif any(ismember(fields(ggrain.prop),'error'))
-%         binaryImg = ggrain.prop.error;
+        %     elseif any(ismember(fields(ggrain.prop),'oldId'))
+        %         binaryImg = ggrain.prop.oldId;
+        %     elseif any(ismember(fields(ggrain.prop),'grainId'))
+        %         binaryImg = ggrain.prop.grainId;
+        %     elseif any(ismember(fields(ggrain.prop),'confidenceindex'))
+        %         binaryImg = ggrain.prop.confidenceindex;
+        %     elseif any(ismember(fields(ggrain.prop),'ci'))
+        %         binaryImg = ggrain.prop.ci;
+        %     elseif any(ismember(fields(ggrain.prop),'fit'))
+        %         binaryImg = ggrain.prop.fit;
+        %     elseif any(ismember(fields(ggrain.prop),'semsignal'))
+        %         binaryImg = ggrain.prop.semsignal;
+        %     elseif any(ismember(fields(ggrain.prop),'mad'))
+        %         binaryImg = ggrain.prop.mad;
+        %     elseif any(ismember(fields(ggrain.prop),'error'))
+        %         binaryImg = ggrain.prop.error;
     end
 
     % replace NaNs with zeros
     binaryImg(isnan(binaryImg)) =  0;
 
-    % pad binary image to the nearest square
-    % From https://au.mathworks.com/matlabcentral/answers/1853683-change-an-image-from-rectangular-to-square-by-adding-white-area
-    nrows = size(binaryImg,1);
-    ncols = size(binaryImg,2);
-    d = abs(ncols-nrows);    % find the difference between ncols and nrows
-    if(mod(d,2) == 1)        % if the difference is an odd number
-        if (ncols > nrows)   % add a row at the end
-            binaryImg = [binaryImg; zeros(1, ncols)];
-            nrows = nrows + 1;
-        else                 % add a col at the end
-            binaryImg = [binaryImg zeros(nrows, 1)];
-            ncols = ncols + 1;
+    if padLogic == true
+        % pad binary image to the nearest square
+        % From https://au.mathworks.com/matlabcentral/answers/1853683-change-an-image-from-rectangular-to-square-by-adding-white-area
+        nrows = size(binaryImg,1);
+        ncols = size(binaryImg,2);
+        d = abs(ncols-nrows);    % find the difference between ncols and nrows
+        if(mod(d,2) == 1)        % if the difference is an odd number
+            if (ncols > nrows)   % add a row at the end
+                binaryImg = [binaryImg; zeros(1, ncols)];
+                nrows = nrows + 1;
+            else                 % add a col at the end
+                binaryImg = [binaryImg zeros(nrows, 1)];
+                ncols = ncols + 1;
+            end
+        end
+        if ncols > nrows
+            binaryImg = padarray(binaryImg, [(ncols-nrows)/2 0]);
+        else
+            binaryImg = padarray(binaryImg, [0 (nrows-ncols)/2]);
         end
     end
-    if ncols > nrows
-        binaryImg = padarray(binaryImg, [(ncols-nrows)/2 0]);
-    else
-        binaryImg = padarray(binaryImg, [0 (nrows-ncols)/2]);
-    end
 
-    % FFT of the greyscale grain (complex)
+    % fft of the greyscale grain (complex)
     fftComplex = fftshift(fft2((binaryImg)));
 
-
-    % FFT of the binarised grain (real)
+    % fft of the binarised grain (real)
     binaryImg(binaryImg>0) =  1;
     fftReal = abs(log2(fftshift(fft2(binaryImg))));
 
-    % save the FFTs to the grain variable
+    % save the ffts to the grain variable
     outgrains.prop.fftComplex{ii,1} = fftComplex;
     outgrains.prop.fftReal{ii,1} = fftReal;
 
