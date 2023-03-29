0a4d9c85bbf5b40a3bfcbdc2ac6a11d27bbf860f
azdiarGazder <azdiar.gazder@gmail.com>
2023-03-30 10:24:37 +1100

    Update demo_pad.m

--- pad/demo_pad.m
+++ pad/demo_pad.m
@@ -1,45 +1,27 @@
 close all; clc; clear all; clear hidden;
 
-%% UN-REMARK EACH SECTION BELOW AND RUN SEPARATELY
 %-----------------
 % define a binary map
-inMap = ones(randi([1,100],1,1), randi([1,50],1,1));
+colmax = 100; romax = 100; 
+inMap = zeros(randi([1,colmax],1,1), randi([1,romax],1,1));
 size(inMap)
-tiledlayout(1,2);
 
-% LHS plot
-nexttile;
-imagesc(inMap); colorbar; caxis([0 1]);
+% plot the input map
+figure; imagesc(inMap); colorbar; caxis([0 1]); 
+% xlim([1 colmax]); ylim([1 romax]); %axis tight
 title('Input map')
 
 % pad the map
-outMap = pad(inMap,[4 1],'zeros');
+%% UN-REMARK EACH COMMAND IN THIS SECTION AND RUN SEPARATELY
+% outMap = pad(inMap,'size',[0 5],'ones');
+outMap = pad(inMap,'square','ones');
+% outMap = pad(inMap,'auto','ones');
+%%
 size(outMap)
 
-% RHS plot
-nexttile;
-imagesc(outMap); colorbar; caxis([0 1]);
+
+% plot the padded map
+figure; imagesc(outMap); colorbar; caxis([0 1]); 
+% xlim([1 100]); ylim([1 50]);
 title('Padded map')
 %-----------------
-
-
-% %-----------------
-% % define a binary map
-% inMap = zeros(randi([1,100],1,1), randi([1,50],1,1));
-% size(inMap)
-% tiledlayout(1,2);
-% 
-% % LHS plot
-% nexttile;
-% imagesc(inMap); colorbar; caxis([0 1]);
-% title('Input map')
-% 
-% % pad the map
-% outMap = pad(inMap,[3 1],'ones');
-% size(outMap)
-% 
-% % RHS plot
-% nexttile;
-% imagesc(outMap); colorbar; caxis([0 1]);
-% title('Padded map')
-% %-----------------
