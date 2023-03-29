9173038866f1fe5bc38887a787b89ff77f9414a0
azdiarGazder <azdiar.gazder@gmail.com>
2023-03-29 15:55:05 +1100

    Update demo_autoPad.m

--- autoPad/demo_autoPad.m
+++ autoPad/demo_autoPad.m
@@ -9,7 +9,7 @@ close all; clc; clear all; clear hidden;
 % 
 % % plot the input map
 % figure; imagesc(inMap); colorbar; caxis([0 1]); 
-% xlim([1 colmax]); ylim([1 romax]); 
+% xlim([1 colmax]); ylim([1 romax]); %axis tight
 % title('Input map')
 % 
 % % pad the map
@@ -28,16 +28,16 @@ close all; clc; clear all; clear hidden;
 % define a binary map
 colmax = 25; romax = 100; 
 inMap = zeros(randi([1,colmax],1,1), randi([1,romax],1,1));
-size(inMap)
+size(inMap);
 
 % plot the input map
 figure; imagesc(inMap); colorbar; caxis([0 1]); 
-xlim([1 colmax]); ylim([1 romax]); 
+xlim([1 colmax]); ylim([1 romax]); %axis tight
 title('Input map')
 
 % pad the map
 outMap = autoPad(inMap,1,'ones');
-size(outMap)
+size(outMap);
 
 % plot the padded map
 figure; imagesc(outMap); colorbar; caxis([0 1]); 
