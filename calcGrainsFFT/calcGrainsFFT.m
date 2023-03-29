6e2bd80eca61d023c2dd6bfa9cb88b6623c86504
azdiarGazder <azdiar.gazder@gmail.com>
2023-03-30 10:54:29 +1100

    Update calcGrainsFFT.m

--- calcGrainsFFT/calcGrainsFFT.m
+++ calcGrainsFFT/calcGrainsFFT.m
@@ -1,10 +1,10 @@
 function outgrains = calcFFT(inebsd,ingrains,varargin)
 %% Function description:
 % Returns the Fast Fourier Transforms (FFTs) of individual grains. Unless
-% specified otherwise, the FFTs are calculated after padding each 
-% grayscale/binary grain map to its nearest square. 
-% The FFTs from grayscale and binary data are returned in grid format 
-% within the 'grains.prop.fftGray' and 'grains.prop.fftBinary' structure 
+% specified otherwise, the FFTs are calculated after padding each
+% grayscale/binary grain map to its nearest square.
+% The FFTs from grayscale and binary data are returned in grid format
+% within the 'grains.prop.fftGray' and 'grains.prop.fftBinary' structure
 % variables.
 %
 %% Author:
@@ -19,9 +19,6 @@ function outgrains = calcFFT(inebsd,ingrains,varargin)
 %
 %% Output:
 %  grains       - @grain2d
-%
-%% Options:
-%  'noPad'     -  @char, specifies no padding of the binary grain map/image
 %%
 
 
@@ -41,14 +38,6 @@ if ~isa(ingrains,'grain2d')
     return;
 end
 
-% check for padding of the binary grain map/image before FFT
-if ~isempty(varargin) && check_option(varargin,'noPad')
-    padLogic = false;
-elseif isempty(varargin) || (~isempty(varargin) && ~check_option(varargin,'noPad'))
-    padLogic = true;
-end
-
-
 
 outgrains = ingrains;
 
@@ -56,7 +45,7 @@ for ii = 1:length(outgrains)
 
     ggrain = gridify(inebsd(ingrains(ii)));
 
-    % create a grid of the grain
+    % create a gridded map using any ebsd property of the grain of interest
     if any(ismember(fields(ggrain.prop),'imagequality'))
         grainMap = ggrain.prop.imagequality;
     elseif any(ismember(fields(ggrain.prop),'iq'))
@@ -69,56 +58,23 @@ for ii = 1:length(outgrains)
         grainMap = ggrain.prop.bandslope;
     elseif any(ismember(fields(ggrain.prop),'bs'))
         grainMap = ggrain.prop.bs;
-        %     elseif any(ismember(fields(ggrain.prop),'oldId'))
-        %         grainMap = ggrain.prop.oldId;
-        %     elseif any(ismember(fields(ggrain.prop),'grainId'))
-        %         grainMap = ggrain.prop.grainId;
-        %     elseif any(ismember(fields(ggrain.prop),'confidenceindex'))
-        %         grainMap = ggrain.prop.confidenceindex;
-        %     elseif any(ismember(fields(ggrain.prop),'ci'))
-        %         grainMap = ggrain.prop.ci;
-        %     elseif any(ismember(fields(ggrain.prop),'fit'))
-        %         grainMap = ggrain.prop.fit;
-        %     elseif any(ismember(fields(ggrain.prop),'semsignal'))
-        %         grainMap = ggrain.prop.semsignal;
-        %     elseif any(ismember(fields(ggrain.prop),'mad'))
-        %         grainMap = ggrain.prop.mad;
-        %     elseif any(ismember(fields(ggrain.prop),'error'))
-        %         grainMap = ggrain.prop.error;
     end
 
     % replace NaNs with zeros
-    % here the grainMap comprises the band contrast values surrounded by 0
+    % here the grainMap comprises greyscale IQ/BC/BS values surrounded by 0s
     grainMap(isnan(grainMap)) =  0;
 
-    % pad binary map to the nearest square (unless specified otherwise)
-    % From https://au.mathworks.com/matlabcentral/answers/1853683-change-an-image-from-rectangular-to-square-by-adding-white-area
-    if padLogic == true
-        nrows = size(grainMap,1);
-        ncols = size(grainMap,2);
-        d = abs(ncols-nrows);    % find the difference between ncols and nrows
-        if(mod(d,2) == 1)        % if the difference is an odd number
-            if (ncols > nrows)   % add a row at the end
-                grainMap = [grainMap; zeros(1, ncols)];
-                nrows = nrows + 1;
-            else                 % add a col at the end
-                grainMap = [grainMap zeros(nrows, 1)];
-                ncols = ncols + 1;
-            end
-        end
-        if ncols > nrows
-            grainMap = padarray(grainMap, [(ncols-nrows)/2 0]);
-        else
-            grainMap = padarray(grainMap, [0 (nrows-ncols)/2]);
-        end
-    end
+    % pad grainMap to the nearest square
+    grainMap = pad(grainMap,'square','zeros');
 
-    % fft of the greyscale data (2D power spectrum)
+    % fft of the square greyscale data (2D power spectrum)
     fftGray = abs(log2(fftshift(fft2(grainMap))));
 
-    % fft of the binary data (2D power spectrum)
-    % here the grainMap comprises all band contrast values = 1 and surrounded by 0
+    % binarise the greyscale data
+    % here the grainMap comprises IQ/BC/BS values = 1 surrounded by 0s
     grainMap(grainMap>0) =  1;
+
+    % fft of the square binary data (2D power spectrum)
     fftBinary = abs(log2(fftshift(fft2(grainMap))));
 
     % save the ffts to the grain variable
