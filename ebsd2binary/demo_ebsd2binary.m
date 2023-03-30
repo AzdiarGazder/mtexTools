76b375ae5968ce3c3e828f7d490acc60b447d6d8
azdiarGazder <azdiar.gazder@gmail.com>
2023-03-30 11:23:36 +1100

    Create demo_ebsd2binary.m

---
+++ ebsd2binary/demo_ebsd2binary.m
@@ -0,0 +1,76 @@
+close all; clc; clear all; clear hidden;
+startup_mtex
+
+%% UN-REMARK EACH SECTION BELOW AND RUN SEPARATELY
+% %-----------------
+% % hexGrid
+% % define the crystal system
+% CS = {'notIndexed',...
+%     crystalSymmetry('m-3m', [3.6 3.6 3.6], 'mineral', 'copper')};
+% % load an mtex dataset
+% mtexdata copper
+% ebsd.CSList = CS;
+% % consider only indexed data
+% ebsd = ebsd('indexed');
+% ebsd = ebsd('copper');
+% [grains,ebsd.grainId] = calcGrains(ebsd,'angle',2*degree);
+% % remove EBSD data of grains < 5 pixels
+% ebsd(grains(grains.grainSize < 5)) = [];
+% % re-calculate the grains from the remaining data
+% [grains,ebsd.grainId] = calcGrains(ebsd,'angle',2*degree);
+% % smooth the grain boundaries
+% grains = smooth(grains,5);
+% % select a grain of interest
+% grainId = 176;
+% %-----------------
+
+
+
+%-----------------
+% squareGrid
+% define the crystal system
+CS = {'notIndexed',...
+    crystalSymmetry('6/mmm', [1.5708 1.5708 2.0944], 'X||a*', 'Y||b', 'Z||c*', 'mineral', 'Mg')};
+% load an mtex dataset
+mtexdata twins
+ebsd.CSList = CS;
+% consider only indexed data
+ebsd = ebsd('indexed');
+ebsd = ebsd('Mg');
+% calculate grains
+[grains,ebsd.grainId] = calcGrains(ebsd,'angle',2*degree);
+% remove EBSD data of grains < 5 pixels
+ebsd(grains(grains.grainSize < 5)) = [];
+% re-calculate the grains from the remaining data
+[grains,ebsd.grainId] = calcGrains(ebsd,'angle',2*degree);
+% smooth the grain boundaries
+grains = smooth(grains,5);
+% select a grain of interest
+grainId = 53;
+%-----------------
+%%
+
+
+%% PLEASE DO NOT MODIFY BELOW THIS LINE 
+%-----------------
+figure; % plot the ebsd map
+plot(ebsd,ebsd.orientations)
+
+figure; % plot the grain map
+plot(grains,grains.meanOrientation)
+
+figure; % plot the ebsd data of the grain of interest
+plot(ebsd(grains(grainId)),ebsd(grains(grainId)).orientations)
+
+% gridify the ebsd data of the grain of interest
+ebsdGrain = gridify(ebsd(grains(grainId)));
+% binarise the grain of interest
+binaryMap1 = ebsd2binary(ebsdGrain,'ones');
+binaryMap2 = ebsd2binary(ebsdGrain,'zeros');
+
+% plot the binary maps of the grain of interest
+figure; 
+imagesc(binaryMap1)
+figure; 
+imagesc(binaryMap2)
+%-----------------
