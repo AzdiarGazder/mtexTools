231683f471df36c68b07c259876ea6eb910c1e68
azdiarGazder <azdiar.gazder@gmail.com>
2023-03-31 14:51:56 +1100

    Update orientationMaker.m

--- orientationMaker/orientationMaker.m
+++ orientationMaker/orientationMaker.m
@@ -15,12 +15,12 @@ function orientationMaker(ori,sampleSymmetry,varargin)
 % https://github.com/ORTools4MTEX/ORTools/blob/develop/orientationMaker.m
 %
 %% Syntax:
-%  fibreMaker(crystalDirection,specimenDirection)
+%  orientationMaker(ori,sampleSymmetry)
 %
 %% Input:
 %  ori                  - @orientation
 %  sampleSymmetry       - @specimenSymmetry
-%  
+%
 %% Options:
 %  halfwidth    - halfwidth for the ODF calculation
 %  points       - number of points (discrete orientations) in the VPSC file
@@ -35,13 +35,30 @@ pfName_Out = get_option(varargin,'export','inputVPSC.Tex');
 sS = specimenSymmetry('triclinic');
 
 % calculate a single orientation ODF with all symmetries
-odf = unimodalODF(symmetrise(ori),'de la Vallee Poussin',...
-    'halfwidth',hwidth,'Fourier',22);
+% check for MTEX version
+currentVersion = 5.9;
+fid = fopen('VERSION','r');
+MTEXversion = fgetl(fid);
+fclose(fid);
+MTEXversion = str2double(MTEXversion(5:end-2));
 
-% re-define the ODF specimen symmetry based on the user's specification
-odf.SS = sampleSymmetry;
+if MTEXversion >= currentVersion % for MTEX versions 5.9.0 and above
+    psi = SO3DeLaValleePoussinKernel('halfwidth',hwidth);
+    SO3F = SO3FunRBF(symmetrise(ori),psi);
+    % re-define the ODF specimen symmetry based on the user specification
+    SO3F.center.SS = sampleSymmetry;
+    % save a VPSC *.tex file
+    export_VPSC(SO3F.center,pfName_Out,'interface','VPSC','Bunge','points',numPoints);
 
-% save a VPSC *.tex file
-export_VPSC(odf,pfName_Out,'interface','VPSC','Bunge','points',numPoints);
+else % for MTEX versions 5.8.2 and below
+    % calculate a unimodal ODF
+    odf = unimodalODF(symmetrise(ori),'de la Vallee Poussin',...
+        'halfwidth',hwidth,'Fourier',22);
+    % re-define the ODF specimen symmetry based on the user specification
+    odf.SS = sampleSymmetry;
+    % save a VPSC *.tex file
+    export_VPSC(odf,pfName_Out,'interface','VPSC','Bunge','points',numPoints);
+end
+
 end
 
