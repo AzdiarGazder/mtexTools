bf821a89dba02e19aad774a136b6c92ea51ec58a
azdiarGazder <azdiar.gazder@gmail.com>
2023-03-31 16:09:37 +1100

    Update fibreMaker.m

--- fibreMaker/fibreMaker.m
+++ fibreMaker/fibreMaker.m
@@ -1,6 +1,6 @@
 function fibreMaker(crystalDirection,sampleDirection,sampleSymmetry,varargin)
 %% Function description:
-% Creates an ideal crystallographic fibre with a user specified 
+% Creates an ideal crystallographic fibre with a user specified
 % half-width and exports the data as a VPSC file for later use.
 %
 %% Author:
@@ -8,19 +8,19 @@ function fibreMaker(crystalDirection,sampleDirection,sampleSymmetry,varargin)
 %
 %% Modified by:
 % Dr. Frank Niessen to include varargin.
-%      
+%
 %% Version(s):
 % The first version of this function was posted in:
 % https://github.com/ORTools4MTEX/ORTools/blob/develop/fibreMaker.m
 %
 %% Syntax:
-%  fibreMaker(crystalDirection,specimenDirection)
+%  fibreMaker(crystalDirection,samplenDirection,,sampleSymmetry)
 %
 %% Input:
 %  crystalDirection     - @Miller
 %  sampleDirection      - @vector3d
 %  sampleSymmetry       - @specimenSymmetry
-%  
+%
 %% Options:
 %  halfwidth    - halfwidth for the ODF calculation
 %  points       - number of points (discrete orientations) in the VPSC file
@@ -29,18 +29,44 @@ function fibreMaker(crystalDirection,sampleDirection,sampleSymmetry,varargin)
 
 hwidth = get_option(varargin,'halfwidth',2.5*degree);
 numPoints = get_option(varargin,'points',1000);
-pfName_Out = get_option(varargin,'export','inputVPSC.Tex');
 
 % define the specimen symmetry to compute ODF
 ss = specimenSymmetry('triclinic');
 
-% calculate a fibre ODF
-odf = fibreODF(crystalDirection,sampleDirection,ss,'de la Vallee Poussin',...
-    'halfwidth',hwidth,'Fourier',22);
+% check for MTEX version
+currentVersion = 5.9;
+fid = fopen('VERSION','r');
+MTEXversion = fgetl(fid);
+fclose(fid);
+MTEXversion = str2double(MTEXversion(5:end-2));
 
-% re-define the ODF specimen symmetry based on the user's specification
-odf.SS = sampleSymmetry;
+if MTEXversion >= currentVersion % for MTEX versions 5.9.0 and above
+    pfName_Out = get_option(varargin,'export','inputFibre.txt');
 
-% save a VPSC *.tex file
-export_VPSC(odf,pfName_Out,'interface','VPSC','Bunge','points',numPoints);
+    % pre-define the fibre
+    f = fibre(crystalDirection,sampleDirection,ss,'full'); 
+    % calculate a fibre ODF
+    odf = fibreODF(f,'de la Vallee Poussin',...
+        'halfwidth',hwidth,'Fourier',22);
+    % re-define the ODF specimen symmetry based on user specification
+    odf.SS = sampleSymmetry;
+    % discretise the ODF based on user specification
+    ori = discreteSample(odf,numPoints);
+    % save an MTEX ASCII File *.txt file (lossless format)
+    export(ori,pfName_Out,'Bunge','interface','mtex');
+
+else % for MTEX versions 5.8.2 and below
+    pfName_Out = get_option(varargin,'export','inputFibre.Tex');
+
+    % calculate a fibre ODF
+    odf = fibreODF(crystalDirection,sampleDirection,ss,'de la Vallee Poussin',...
+        'halfwidth',hwidth,'Fourier',22);
+    % re-define the ODF specimen symmetry based on user specification
+    odf.SS = sampleSymmetry;
+    % save a VPSC *.Tex file (lossy format)
+    export_VPSC(odf,pfName_Out,'interface','VPSC','Bunge','points',numPoints);
+end
+
+
+
 end
