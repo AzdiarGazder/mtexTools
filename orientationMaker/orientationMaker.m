b2b4023b3035826bdf89c858fe1a1f7a8980ffbb
azdiarGazder <azdiar.gazder@gmail.com>
2023-04-12 13:17:38 +1000

    Update orientationMaker.m

--- orientationMaker/orientationMaker.m
+++ orientationMaker/orientationMaker.m
@@ -1,8 +1,8 @@
-function orientationMaker(ori,sampleSymmetry,varargin)
+function orientationMaker2(oriIn,sampleSymmetry,varargin)
 %% Function description:
 % Creates an ideal crystallographic orientation from a unimodal ODF with a
-% user specified half-width and exports the data as a lossless Mtex 
-% (version 5.9 onwards) or lossy VPSC (up to version 5.8.2) file for 
+% user specified half-width and exports the data as a lossless Mtex
+% (version 5.9 onwards) or lossy VPSC (up to version 5.8.2) file for
 % later use.
 %
 %% Author:
@@ -19,7 +19,7 @@ function orientationMaker(ori,sampleSymmetry,varargin)
 %  orientationMaker(ori,sampleSymmetry)
 %
 %% Input:
-%  ori                  - @orientation
+%  oriIn                - @orientation
 %  sampleSymmetry       - @specimenSymmetry
 %
 %% Options:
@@ -35,47 +35,22 @@ numPoints = get_option(varargin,'points',1000);
 sS = specimenSymmetry('triclinic');
 
 % calculate a single orientation ODF with all symmetries
-% check for MTEX version
-currentVersion = 5.9;
-fid = fopen('VERSION','r');
-MTEXversion = fgetl(fid);
-fclose(fid);
-MTEXversion = str2double(MTEXversion(5:end-2));
+pfName_Out = get_option(varargin,'export','inputOrN.txt');
 
-if MTEXversion >= currentVersion % for MTEX versions 5.9.0 and above
-    pfName_Out = get_option(varargin,'export','inputOrN.txt');
+% calculate a unimodal ODF
+odf = unimodalODF(symmetrise(oriIn),'halfwidth',hwidth);
 
-    psi = SO3vonMisesFisherKernel('halfwidth',1.225*hwidth);
-    % calculate a unimodal ODF
-    odf = unimodalODF(symmetrise(oriIn),psi);
-    % discretise the ODF based on user specification
-    oriOut = discreteSample(odf,numPoints);
-    % save an MTEX ASCII File *.txt file (lossless format)
-    export(oriOut,pfName_Out,'Bunge','interface','mtex');
+% re-define the ODF specimen symmetry based on the user specification
+odf.SS = sampleSymmetry;
 
-%     psi = SO3DeLaValleePoussinKernel('halfwidth',hwidth);
-%     SO3F = SO3FunRBF(symmetrise(ori),psi);
-%     % re-define the ODF specimen symmetry based on the user specification
-%     SO3F.center.SS = sampleSymmetry;
-%     % discretise the ODF based on user specification
-%     ori = discreteSample(SO3F.center,numPoints);
-%     % save an MTEX ASCII File *.txt file (lossless format)
-%     export(ori,pfName_Out,'Bunge','interface','mtex');
+% generate the user specified number of orientations from the ODF
+oriOut = odf.discreteSample(numPoints);
 
-%     % save a VPSC *.tex file
-%     export_VPSC(SO3F.center,pfName_Out,'interface','VPSC','Bunge','points',numPoints);
+% save an MTEX ASCII File *.txt file (lossless format)
+export(oriOut,pfName_Out,'Bunge','interface','mtex');
 
-else % for MTEX versions 5.8.2 and below
-    pfName_Out = get_option(varargin,'export','inputOrN.Tex');
-
-    % calculate a unimodal ODF
-    odf = unimodalODF(symmetrise(ori),'de la Vallee Poussin',...
-        'halfwidth',hwidth,'Fourier',22);
-    % re-define the ODF specimen symmetry based on the user specification
-    odf.SS = sampleSymmetry;
-    % save a VPSC *.Tex file (lossy format)
-    export_VPSC(odf,pfName_Out,'interface','VPSC','Bunge','points',numPoints);
-end
+% % save a VPSC *.Tex file (lossy format)
+% export_VPSC(odf,pfName_Out,'interface','VPSC','Bunge','points',numPoints);
 
 end
 
