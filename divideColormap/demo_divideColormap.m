66af85c50df563d0ce7bcbd23274d95e92f857f4
azdiarGazder <azdiar.gazder@gmail.com>
2023-05-09 11:56:12 +1000

    Create demo_divideColormap.m

---
+++ divideColormap/demo_divideColormap.m
@@ -0,0 +1,18 @@
+close all; clc; clear all; clear hidden;
+
+% visualise the default color palette of the colormap
+figure(1)
+% define a colormap to use
+cmap = colormap('jet');
+colorbar;
+
+
+
+% visualise a discreted color palette of the colormap
+figure(2)
+% sub-divide a colormap into a user specified number of discrete colors
+% cmap = divideColormap(colormap('jet'),5);
+cmap = divideColormap(cmap,5);
+% define the discretised colormap
+colormap(cmap);
+colorbar;
