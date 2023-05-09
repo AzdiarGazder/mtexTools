82160710b400dd5e71a0bc6e2969ff298a0e3ec3
azdiarGazder <azdiar.gazder@gmail.com>
2023-05-09 14:06:39 +1000

    Update demo_discreteColormap.m

--- discreteColormap/demo_discreteColormap.m
+++ discreteColormap/demo_discreteColormap.m
@@ -11,8 +11,17 @@ colorbar;
 % visualise a discreted color palette of the colormap
 figure(2)
 % sub-divide a colormap into a user specified number of discrete colors
-% cmap = divideColormap(cmap,5);
+% cmap = discreteColormap(cmap,5);
 cmap = discreteColormap(colormap('jet'),5);
 % define the discretised colormap
 colormap(cmap);
+colorbar;
+
+
+
+% visualise a discreted color palette of the colormap
+figure(3)
+% define the discretised colormap by sub-dividing a default colormap into 
+% a user specified number of discrete colors
+colormap(discreteColormap(jet,5));
 colorbar;
