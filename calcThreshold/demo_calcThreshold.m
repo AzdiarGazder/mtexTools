574f24fdd35182f22b28e7438d047b01edbf4063
azdiarGazder <azdiar.gazder@gmail.com>
2023-11-27 15:18:59 +1100

    Update demo_calcThreshold.m

--- calcThreshold/demo_calcThreshold.m
+++ calcThreshold/demo_calcThreshold.m
@@ -1,20 +1,23 @@
 clc; clear all; clear hidden; close all;
 
+%% DEMO SET #1
 % Load a demonstration dataset
-% load("dataset.mat");
-% diameters = dataset(:,4); % grain feret diameter
-
 load("grainSize.mat");
 
+% Threshold the PDF
+out = calcThreshold(grainSize_rcrx,'pdf','binWidth',0.01,'sigma',3)
+out.x(out.id)
+
+
+%% DEMO SET #2
+% Load a demonstration dataset
+load("dataset.mat");
+diameters = dataset(:,4); % grain feret diameter
 
 % Threshold the cumulative distribution function (CDF)
-% out = calcThreshold(diameters,'scott','sigma',3)
+out = calcThreshold(diameters,'scott','sigma',3)
 % which is equivalent to
-% out = calcThreshold(diameters,'cdf','scott','sigma',3)
+%  out = calcThreshold(diameters,'cdf','scott','sigma',3)
 
 % Threshold the CDF differential
-% out = calcThreshold(diameters,'dcdf','binWidth',10,'sigma',3)
-
-out = calcThreshold(grainSize_rcrx,'pdf','binWidth',0.01,'sigma',3)
-
-out.x(out.id)
+out = calcThreshold(diameters,'dcdf','binWidth',10,'sigma',3)
