34cd3f2f82dd945e19ec6c3fe0a6146a54abaec9
azdiarGazder <azdiar.gazder@gmail.com>
2023-12-12 12:52:34 +1100

    Minor edits

--- rcrxSegmenter/calcClusters.m
+++ rcrxSegmenter/calcClusters.m
@@ -1,7 +1,34 @@
 function out = calcClusters(inData,varargin)
+%% Function description:
+% This function is a simple implementation of Gaussian Mixture Modelling 
+% used for hard clustering. 
+%
+%% Modified by:
+% Dr. Azdiar Gazder, 2023, azdiaratuowdotedudotau
+%
+%% Author: 
+% Dr. George Azzopardi
+% University of Malta
+% CSA3220 - Machine Learning, Expert Systems, & Fuzzy Logic
+% For the script published in the MATLAB file exchange:
+% https://au.mathworks.com/matlabcentral/fileexchange/54568-clustering-with-gaussian-mixture-models-gmms
+%
+%% Syntax:
+%  calcClusters(data)
+%
+%% Input:
+%  data         - @double, a n x 1 array
+%
+%% Options:
+%  'clusters'   - @double, defines the number of clusters
+%  'tolerance'  - @char, defines the error tolerance for convergence
+%
+%%
+
 
 numClusters = get_option(varargin,'clusters',2);
 errTol = get_option(varargin,'tolerance',1E-6);
+
 
 [ro, col] = size(inData); % size of the input data array
 
@@ -29,7 +56,7 @@ while t == 1 || ~convergence % convergence criteria
         post(nn,:) = post(nn,:)./ sum(post(nn,:));
     end
 
-    %% Step 3: Maximisation - Update model parameters (means, covariances
+    %% Step 3: Maximisation - Update model parameters (means, covariance
     % matrices and weights
     for kk = 1:numClusters
         % Update the means
@@ -45,6 +72,7 @@ while t == 1 || ~convergence % convergence criteria
         end
         sigma{kk} = sigma{kk}./sp(kk);
     end
+    
     % Update the weights
     weight = sp./sum(sp);
 
@@ -65,6 +93,7 @@ while t == 1 || ~convergence % convergence criteria
     t = t + 1;
 end
 
+%% Step 5: Assemble the output
 out.model.means = mu;
 out.model.covariances = sigma;
 out.model.weights = weight;
@@ -78,3 +107,5 @@ for ii = 1:numClusters
                                           % cluster membership
 end
 % ------
+
+end
--- rcrxSegmenter/demo_rcrxSegmenter_gaussMixture.m
+++ rcrxSegmenter/demo_rcrxSegmenter_gaussMixture.m
@@ -42,7 +42,7 @@ startup_mtex;
 setMTEXpref('xAxisDirection','east');
 setMTEXpref('zAxisDirection','intoPlane');
 setMTEXpref('maxSO3Bandwidth',96);
-currentFolder;
+
 
 
 %% Define the critical angle for a high-angle boundary
@@ -125,7 +125,7 @@ set(figH,'Name','Recrystallised ebsd fraction','NumberTitle','on');
 
 
 
-
+return
 %% NEW STEP 2: Simultanesouly apply the aspect ratio and shape factor
 % criteria to the unrecrystallised fraction
 aR_unrcrx = aspectRatio(grains_unrcrx);
