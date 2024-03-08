738c83053ba97ea003cb2d82851a079190dc0050
Azdiar.Gazder <azdiar.gazder@gmail.com>
2024-03-08 15:10:42 +1100

    Major improvements

--- plotMcKenzie/demo_plotMcKenzie.m
+++ plotMcKenzie/demo_plotMcKenzie.m
@@ -6,8 +6,18 @@ startup_mtex;
 setMTEXpref('FontSize',14);
 set(0,'DefaultFigureWindowStyle','normal');
 
-% Define the crystal system
-CS = crystalSymmetry('m-3m', [2.9 2.9 2.9], 'mineral', 'bcc');
+% Define the crystal system(s)
+CS1 = crystalSymmetry('m-3m', [2.9 2.9 2.9], 'mineral', 'bcc');
+CS2 = crystalSymmetry('6/mmm', [2.5 2.5 4.1], 'X||a*', 'Y||b', 'Z||c*', 'mineral', 'hcp');
+CS3 = crystalSymmetry('4/mmm', [8.8 8.8 4.5], 'mineral', 'tet');
 
-% Return the McKenzie distribution
-h = plotMcKenzie(1E5,CS)
+% Return the McKenzie distribution of the bcc CS
+% h = plotMcKenzie(1E5,CS1)
+% h = plotMcKenzie(1E5,CS1,CS1)
+
+% Return the McKenzie distribution of the bcc and hcp CS
+h = plotMcKenzie(1E5,CS1,CS2)
+
+
+
+
--- plotMcKenzie/plotMcKenzie.m
+++ plotMcKenzie/plotMcKenzie.m
@@ -1,15 +1,15 @@
-function figH = plotMcKenzie(n,CS,varargin)
+function figH = plotMcKenzie(n,varargin)
 %% Function description:
-% Plots the McKenzie distribution of a crystal system for a user-defined 
-% number of orientations in publication-ready format. It returns the 
-% histogram data as well as MTEX's default McKenzie distribtuon after 
-% scaling.
+% Plots the McKenzie distribution of 1 or 2 crystal systems for a 
+% user-defined number of orientations in publication-ready format. It 
+% returns the histogram data as well as MTEX's default McKenzie distribtuon
+% after scaling.
 %
 %% Author:
 % Dr. Azdiar Gazder, 2023, azdiaratuowdotedudotau
 %
 %% Syntax:
-%  plotPF(n,CS,varargin) 
+%  plotMcKenzie(n,CS,varargin)
 %
 %% Input:
 %  n               - @double
@@ -21,19 +21,43 @@ function figH = plotMcKenzie(n,CS,varargin)
 %%
 
 
-% Define the fundamental region of the crystal symmetry
-fR = fundamentalRegion(CS);
-% Calculate the maximum misorientation of the crystal symmetry
-maxAngle = fR.maxAngle./degree; 
+% Apply the class function to each element of varargin
+vargTypes = cellfun(@class, varargin, 'UniformOutput', false);
 
-% Define n - number of random orientations
-ori1 = orientation.rand(n,CS);
-ori2 = orientation.rand(n,CS);
+% Return a logical array for search string matches
+matchIdx = strcmp(vargTypes, 'crystalSymmetry');
+% Find the column indices of the matches
+[~, col] = find(matchIdx == 1);
+% Count the number of matches
+numMatch = numel(col);
+
+if numMatch == 1
+    CS = varargin{col(1)};
+    % Define the fundamental region of the crystal symmetry
+    fR = fundamentalRegion(CS);
+    % Define n - number of random orientations
+    ori1 = orientation.rand(n,CS);
+    ori2 = orientation.rand(n,CS);
+elseif numMatch == 2
+    CS1 = varargin{col(1)};
+    CS2 = varargin{col(2)};
+    % Define the fundamental region of the crystal symmetry
+    fR = fundamentalRegion(CS1,CS2);
+    % Define n - number of random orientations
+    ori1 = orientation.rand(n,CS1);
+    ori2 = orientation.rand(n,CS2);
+elseif numMatch > 2
+    error('A maximum of two crystal symmetries are allowed.');
+end
+
+% Calculate the maximum misorientation of the crystal symmetry
+maxAngle = fR.maxAngle./degree;
+
 % Calculate the misorientation angle between the random orientations
 miso = (inv(ori1).*ori2);
 miso = miso.angle./degree;
 
-% Calculate the McKenzie historgram
+% Calculate the McKenzie histogram
 numBins = get_option(varargin,'bins',length(1:ceil(maxAngle)));
 binWidth = ceil(maxAngle) / numBins;
 % Bin the data using the specified bin width and number of bins
@@ -43,11 +67,15 @@ binCenters = binEdges(1:end-1) + binWidth/2;
 % Calculate the probability distribution function (PDF)
 pdf = counts ./ (sum(counts) * binWidth);
 
-
+disp('boo3')
 figH = figure;
-% Plot MTEX's default McKenzie distribution while scaling its y-axis data 
+% Plot MTEX's default McKenzie distribution while scaling its y-axis data
 % to the number of orientations
-h1 = plotAngleDistribution(CS,CS,'numBins',numBins);
+if numMatch == 1
+    h1 = plotAngleDistribution(CS,CS,'numBins',numBins);
+elseif numMatch == 2
+    h1 = plotAngleDistribution(CS1,CS2,'numBins',numBins);
+end
 hold all;
 % Plot the misorientation histogram based on the user defined number of
 % bins
