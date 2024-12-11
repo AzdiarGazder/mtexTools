cf078832806f65c948861ae3bf0587bb796ec30c
Azdiar.Gazder <azdiar.gazder@gmail.com>
2024-12-11 11:12:42 +1100

    Update demo_calcNeighbors.m

--- calcNeighbors/demo_calcNeighbors.m
+++ calcNeighbors/demo_calcNeighbors.m
@@ -12,7 +12,7 @@ grains = smooth(grains,5);
 pairs = grains.neighbors;
 
 %% Calculate the first-order neighbours of grain 74
-neighbors = calcNeighbors(74, pairs, 'order', 1,'include')
+neighbors = calcNeighbors(74, pairs, 'order', 1)
 
 % Highlight the neighbours of grain 74
 plot(grains,grains.meanOrientation); % plot all grains
@@ -30,13 +30,20 @@ hold off
 numGrains = length(grains);
 
 % Pre-allocate a cell array for storing neighbours of each grain
-allNeighbours = cell(numGrains, 1);
+allNeighbours_thirdOrder = cell(numGrains, 1);
+allNeighbours_first2thirdOrder = cell(numGrains, 1);
 
 % Loop through each grain and compute its neighbours
+tic
 for ii = 1:numGrains
     % Get the grain Id
     grainId = grains(ii).id;
+   
     % Calculate only the third-order neighbours of the current grain
-    allNeighbours{ii} = calcNeighbors(grainId, pairs, 'order',3,'exclude');
+    allNeighbours_thirdOrder{ii} = calcNeighbors(grainId, pairs, 'order',3);
+
+    % Calculate the first, second and third -order neighbours of the current grain
+    allNeighbours_first2thirdOrder{ii} = calcNeighbors(grainId, pairs, 'order',3,'include');
 end
+toc
 %%
